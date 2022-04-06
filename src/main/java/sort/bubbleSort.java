b3b8fc17565cc821c60cb096958996f62671c432
chezhijun <chezhijun@58.com>
2022-04-06 09:55:51 +0800

    leetcode

--- src/main/java/sort/bubbleSort.java
+++ src/main/java/sort/bubbleSort.java
@@ -8,28 +8,28 @@ package sort;
 public class bubbleSort {
 
     public static void main(String[] args) {
-        int [] a = new int[]{1,3,5,6,7,4,22,104,33,35};
-
+        int[] a = new int[]{1, 3, 5, 6, 7, 4, 22, 104, 33, 35};
+        int[] bubbleSort = bubbleSort(a);
+        System.out.println(bubbleSort);
     }
 
 
     /**
      * 冒泡排序
+     *
      * @param target
      * @return
      */
-    public static int[] bubbleSort(int[] target){
-        int temp =0;
-        for (int i = 0; i < target.length; i++) {
-            for (int j = 0; j < target.length-1-i; j++) {
-                if (target[j]>target[j+1]){
-                    temp = target[j];
-                    target[j] = target[j=1];
-                    target[j+1] = temp;
+    public static int[] bubbleSort(int[] target) {
+        for (int i = 0; i < target.length - 1; i++) {
+            for (int j = 0; j < target.length - 1 - i; j++) {
+                if (target[j] > target[j + 1]) {
+                    int temp = target[j];
+                    target[j] = target[j + 1];
+                    target[j + 1] = temp;
                 }
             }
         }
-        
         return target;
     }
 }
