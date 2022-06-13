d239b87c64bc84754d63480172b78007f742ae2b
chezhijun <1208370852@qq.com>
2022-06-14 00:06:56 +0800

    re1

--- src/main/java/leetcode/array/Problem1.java
+++ src/main/java/leetcode/array/Problem1.java
@@ -61,4 +61,18 @@ class Solution1 {
         }
         return null;
     }
+
+
+    public int[] reTwoSum(int[] nums ,int target){
+        for (int i = 0; i < nums.length; i++) {
+            for (int j =i+1; j< nums.length;j++){
+                if (nums[j]==target-nums[i]){
+                    return new int[]{i,j};
+                }
+            }
+        }
+
+
+        return null;
+    }
 }
