2e8c3b0fee5e8289f9ee50dab9e50c5b924cccec
chezhijun <1208370852@qq.com>
2022-07-26 00:00:46 +0800

    LeetCode

--- src/main/java/leetcode/array/Array1_500/Problem36.java
+++ src/main/java/leetcode/array/Array1_500/Problem36.java
@@ -1,10 +1,13 @@
 package leetcode.array.Array1_500;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * leetcode36：有效的数独
+ * AC
  */
 public class Problem36 {
 
@@ -25,81 +28,29 @@ public class Problem36 {
     }
 
     public static boolean isValidSudoku(char[][] board) {
-        if (board.length != 9 || board[0].length != 9) {
-            return false;
+        Map<Integer, Set<Integer>> row = new HashMap<>(), col = new HashMap<>(), area = new HashMap<>();
+        for (int i = 0; i < 9; i++) {
+            row.put(i,new HashSet<>());
+            col.put(i,new HashSet<>());
+            area.put(i,new HashSet<>());
         }
-        //横向Map
-        Map<Character, Integer> horizontalline0 = new HashMap<>();
-        Map<Character, Integer> horizontalline1 = new HashMap<>();
-        Map<Character, Integer> horizontalline2 = new HashMap<>();
-        Map<Character, Integer> horizontalline3 = new HashMap<>();
-        Map<Character, Integer> horizontalline4 = new HashMap<>();
-        Map<Character, Integer> horizontalline5 = new HashMap<>();
-        Map<Character, Integer> horizontalline6 = new HashMap<>();
-        Map<Character, Integer> horizontalline7 = new HashMap<>();
-        Map<Character, Integer> horizontalline8 = new HashMap<>();
-        //索引Map
-        Map<Integer, Map<Character, Integer>> indexHorizontal = new HashMap<>();
-        indexHorizontal.put(0, horizontalline0);
-        indexHorizontal.put(1, horizontalline1);
-        indexHorizontal.put(2, horizontalline2);
-        indexHorizontal.put(3, horizontalline3);
-        indexHorizontal.put(4, horizontalline4);
-        indexHorizontal.put(5, horizontalline5);
-        indexHorizontal.put(6, horizontalline6);
-        indexHorizontal.put(7, horizontalline7);
-        indexHorizontal.put(8, horizontalline8);
 
-
-        //纵向MAP
-        Map<Character, Integer> verticalLine0 = new HashMap<>();
-        Map<Character, Integer> verticalLine1 = new HashMap<>();
-        Map<Character, Integer> verticalLine2 = new HashMap<>();
-        Map<Character, Integer> verticalLine3 = new HashMap<>();
-        Map<Character, Integer> verticalLine4 = new HashMap<>();
-        Map<Character, Integer> verticalLine5 = new HashMap<>();
-        Map<Character, Integer> verticalLine6 = new HashMap<>();
-        Map<Character, Integer> verticalLine7 = new HashMap<>();
-        Map<Character, Integer> verticalLine8 = new HashMap<>();
-        //索引Map
-        Map<Integer, Map<Character, Integer>> indexVertical = new HashMap<>();
-        indexVertical.put(0, verticalLine0);
-        indexVertical.put(1, verticalLine1);
-        indexVertical.put(2, verticalLine2);
-        indexVertical.put(3, verticalLine3);
-        indexVertical.put(4, verticalLine4);
-        indexVertical.put(5, verticalLine5);
-        indexVertical.put(6, verticalLine6);
-        indexVertical.put(7, verticalLine7);
-        indexVertical.put(8, verticalLine8);
-
-        for (int i1 = 0; i1 < board.length; i1++) {
-            char[] chars = board[i1];
-            for (int i2 = 0; i2 < chars.length; i2++) {
-                //横向判断
-                if (indexHorizontal.get(i2).containsKey(chars[i2])) {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                char c = board[i][j];
+                if (c=='.'){
+                    continue;
+                }
+                int u = c-'0';
+                int idnx = i/3*3 + j/3;
+                if (row.get(i).contains(u) || col.get(j).contains(u) || area.get(idnx).contains(u)){
                     return false;
-                } else {
-                    indexHorizontal.get(i2).put(chars[i2], i2);
                 }
-                //纵向判断
-                if (indexVertical.get(i2).containsKey(chars[i2])) {
-                    return false;
-                } else {
-                    indexVertical.get(i2).put(chars[i2], i2);
-                }
-                //九宫格判断
-                if (i2 + 1 % 3 == 0 && i1 + 1 % 3 == 0) {
-                    if (i2 < 3 && i1<3) {
-
-                    } else if (i2 < 6 && i1<6) {
-
-                    } else if (i2 < 9 && i1<9) {
-
-                    }
-                }
+                row.get(i).add(u);
+                col.get(j).add(u);
+                area.get(idnx).add(u);
             }
         }
-        return false;
+        return true;
     }
 }
---
+++ src/main/java/leetcode/array/Array1_500/Problem66.java
@@ -0,0 +1,50 @@
+package leetcode.array.Array1_500;
+
+/**
+ * LeetCode66：加一
+ */
+public class Problem66 {
+
+    public static void main(String[] args) {
+        int[] ints = {9,9,9,9,9,9,9};
+        plusOne(ints);
+    }
+
+
+
+    public static int[] plusOne(int[] digits) {
+        if (digits.length == 0) {
+            return digits;
+        }
+        int digit = digits[digits.length - 1];
+        int point = digits.length - 1;
+
+        if (digit != 9) {
+            digits[digits.length - 1] = digit + 1;
+            return digits;
+        }
+
+
+        for (; point >=0; point--) {
+            if (digits[point] != 9) {
+                break;
+            }
+        }
+        if (point != -1) {
+            digits[point] = digits[point] + 1;
+            for (int i = point+1; i < digits.length; i++) {
+                digits[i] = 0;
+            }
+            return digits;
+        } else {
+            int[] ints = new int[digits.length + 1];
+            ints[0] = 1;
+            /*for (int i = 1; i < ints.length; i++) {
+                ints[i] = 0;
+            }*/
+            return ints;
+        }
+
+
+    }
+}
