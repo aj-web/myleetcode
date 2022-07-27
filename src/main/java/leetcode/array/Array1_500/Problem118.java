a354b19d1f520a78fed331a2fd79cb9924942d01
chezhijun <zhjche@rxhui.com>
2022-07-27 17:33:47 +0800

    leetcode118

---
+++ src/main/java/leetcode/array/Array1_500/Problem118.java
@@ -0,0 +1,31 @@
+package leetcode.array.Array1_500;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Problem118 {
+    public static void main(String[] args) {
+
+    }
+
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (numRows==0){
+            return result;
+        }
+
+        for (int i = 1; i <= numRows; i++) {
+            if (i==0){
+                result.add(Arrays.asList(0));
+                continue;
+            }
+
+
+
+        }
+        return result;
+
+    }
+
+}
