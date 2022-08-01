e59c95d77a87afbb3a0323ed494035a6c1f778ad
chezhijun <1208370852@qq.com>
2022-08-01 23:54:01 +0800

    LeetCode

--- src/main/java/leetcode/array/Array1_500/Problem228.java
+++ src/main/java/leetcode/array/Array1_500/Problem228.java
@@ -10,14 +10,13 @@ import java.util.List;
 public class Problem228 {
 
     public static void main(String[] args) {
-        int[] nums = new int[]{0,1,2,4,5,7};
-        System.out.println(summaryRanges(nums));
+        int[] nums = new int[]{0,2,3,4,6,8,9};
+        System.out.println(summaryRanges1(nums));
     }
 
     public static List<String> summaryRanges(int[] nums) {
         List<String> result = new ArrayList<>();
         if (nums.length==0){
-            result.add("0");
             return result;
         }
         if (nums.length==1){
@@ -47,24 +46,34 @@ public class Problem228 {
     }
 
 
-    public List<String> summaryRanges1(int[] nums) {
-        List<String> ret = new ArrayList<String>();
-        int i = 0;
-        int n = nums.length;
-        while (i < n) {
-            int low = i;
-            i++;
-            while (i < n && nums[i] == nums[i - 1] + 1) {
-                i++;
-            }
-            int high = i - 1;
-            StringBuffer temp = new StringBuffer(Integer.toString(nums[low]));
-            if (low < high) {
-                temp.append("->");
-                temp.append(Integer.toString(nums[high]));
-            }
-            ret.add(temp.toString());
+    public static List<String> summaryRanges1(int[] nums) {
+        List<String> result = new ArrayList<>();
+        if (nums.length==0){
+            return result;
         }
-        return ret;
+        if (nums.length==1){
+            result.add(String.valueOf(nums[0]));
+            return result;
+        }
+
+        int left =0;
+        int right = 0;
+
+        while(right< nums.length){
+            right++;
+            while (right< nums.length && nums[right-1]+1==nums[right]){
+                right++;
+            }
+
+            StringBuffer temp = new StringBuffer(Integer.toString(nums[left]));
+            if (left<right-1){
+                temp.append("->");
+                temp.append(nums[right-1]);
+            }
+            result.add(temp.toString());
+            left=right;
+
+        }
+        return result;
     }
 }
