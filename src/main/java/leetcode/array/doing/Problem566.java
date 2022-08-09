f18c9e24f070b765aea7fa53088cf861351e757e
chezhijun <1208370852@qq.com>
2022-08-10 00:11:13 +0800

    LeetCode

---
+++ src/main/java/leetcode/array/doing/Problem566.java
@@ -0,0 +1,23 @@
+package leetcode.array.doing;
+
+public class Problem566 {
+
+
+    public int[][] matrixReshape(int[][] mat, int r, int c) {
+        int m  = mat.length;
+        int n = mat[0].length;
+
+        if(m*n!=r*c){
+            return mat;
+        }
+
+        int[][] res = new int[r][c];
+
+        for (int i = 0; i < m * n; i++) {
+            res[i/c][i%c] = mat[i/n][i%n];
+        }
+
+        return res;
+
+    }
+}
---
+++ src/main/java/leetcode/array/doing/Problem575.java
@@ -0,0 +1,52 @@
+package leetcode.array.doing;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Problem575 {
+
+    public static void main(String[] args) {
+        int[] p = new int[]{1,1,2,3};
+        System.out.println(distributeCandies1(p));
+    }
+
+
+    public static int distributeCandies(int[] candyType) {
+
+        int n = candyType.length/2;
+        Set<Integer> temp = new HashSet<>(candyType.length);
+        for (int i : candyType) {
+            temp.add(i);
+        }
+        if (n>=temp.size()){
+            return temp.size();
+        }else {
+            return n;
+        }
+    }
+
+
+    /**
+     * 有序数组才能使用双指针
+     * @param candyType
+     * @return
+     */
+    public static int distributeCandies1(int[] candyType) {
+
+        int slow = 0;
+        int fast = 0;
+        int target = candyType.length/2;
+        while (fast<candyType.length){
+            if (candyType[fast]!=candyType[slow]){
+                slow++;
+                candyType[slow] = candyType[fast];
+            }
+            fast++;
+            if (slow+1>=target){
+                return slow+1;
+            }
+        }
+        return slow+1;
+
+    }
+}
