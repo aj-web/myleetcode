e0995b08acce6f4960cc0cd8d271582f9649ebd5
chezhijun <zhjche@rxhui.com>
2022-08-26 17:39:43 +0800

    leetcode

--- src/main/java/leetcode/array/doing/Problem717.java
+++ src/main/java/leetcode/array/doing/Problem717.java
@@ -6,5 +6,31 @@ package leetcode.array.doing;
  */
 public class Problem717 {
 
+    public static void main(String[] args) {
+        System.out.println(isOneBitCharacter(new int[]{1, 1, 1, 0}));
+    }
 
+    public static boolean isOneBitCharacter(int[] bits) {
+        int length = bits.length;
+        if (length ==0) {
+            return false;
+        }
+        if (length ==1){
+            return true;
+        }
+
+        int count =0;
+
+        //题目固定结尾为0
+        for (int i = length-2; i >= 0; i--) {
+            if (bits[i]==0){
+                break;
+            }
+            if (bits[i]==1) {
+             count++;
+            }
+        }
+        return count % 2 == 0;
+
+    }
 }
