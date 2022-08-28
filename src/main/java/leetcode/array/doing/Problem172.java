f1208f823795e4754b3816792987669e500bbb06
chezhijun <1208370852@qq.com>
2022-08-28 20:36:51 +0800

    leetcode

---
+++ src/main/java/leetcode/array/doing/Problem172.java
@@ -0,0 +1,18 @@
+package leetcode.array.doing;
+
+/**
+ * 阶乘后的零
+ *
+ * @author : chezj
+ * @date : 2022/8/28 20:04
+ */
+public class Problem172 {
+
+    public int trailingZeroes(int n) {
+        int res = 0;
+        for (int i = n; i / 5 > 0; i = i / 5) {
+            res += i / 5;
+        }
+        return res;
+    }
+}
