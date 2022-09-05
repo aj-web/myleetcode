dc25b9d98157d421f561f5bdcd7ee9d8e833feae
chezhijun <zhjche@rxhui.com>
2022-09-05 09:34:12 +0800

    leetcode

--- src/main/java/leetcode/array/doing/Problem929.java
+++ src/main/java/leetcode/array/doing/Problem929.java
@@ -13,8 +13,10 @@ public class Problem929 {
         Set<String> emailSet = new HashSet<String>();
         for (String email : emails) {
             int i = email.indexOf('@');
-            String local = email.substring(0, i).split("\\+")[0]; // 去掉本地名第一个加号之后的部分
-            local = local.replace(".", ""); // 去掉本地名中所有的句点
+            // 去掉本地名第一个加号之后的部分
+            String local = email.substring(0, i).split("\\+")[0];
+            // 去掉本地名中所有的句点
+            local = local.replace(".", "");
             emailSet.add(local + email.substring(i));
         }
         return emailSet.size();
