6fcb289d3fffc80c69dce01096052088931531f9
chezhijun <1208370852@qq.com>
2022-09-12 15:28:31 +0800

    leetcode

---
+++ src/main/java/leetcode/array/doing/Problem669.java
@@ -0,0 +1,19 @@
+package leetcode.array.doing;
+
+import javax.swing.tree.TreeNode;
+
+/**
+ * @author : chezj
+ * @date : 2022/9/11 14:29
+ */
+public class Problem669 {
+
+
+
+    public TreeNode trimBST(TreeNode root, int low, int high) {
+
+
+        return null;
+    }
+
+}
---
+++ src/main/java/leetcode/array/doing/Problem857.java
@@ -0,0 +1,19 @@
+package leetcode.array.doing;
+
+/**
+ * @author : chezj
+ * @date : 2022/9/11 14:31
+ */
+public class Problem857 {
+
+
+
+    public double mincostToHireWorkers(int[] quality, int[] wage, int k) {
+
+
+
+        return 0L;
+//        return null
+
+    }
+}
