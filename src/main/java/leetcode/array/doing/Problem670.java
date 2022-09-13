4b96ef0b6c5b0194a6ae9ea18c0a3183eec495b2
chezhijun <1208370852@qq.com>
2022-09-13 23:18:42 +0800

    leetcode

--- src/main/java/leetcode/array/doing/Problem670.java
+++ src/main/java/leetcode/array/doing/Problem670.java
@@ -4,7 +4,9 @@ import org.apache.commons.lang3.time.StopWatch;
 
 import java.util.Stack;
 
-/**最大交换
+/**
+ * 最大交换
+ *
  * @author : chezj
  * @date : 2022/9/13 17:49
  */
@@ -12,40 +14,62 @@ public class Problem670 {
     public static void main(String[] args) {
         StopWatch stopWatch = new StopWatch();
         stopWatch.start();
-        Problem670  p = new Problem670();
-        System.out.println(p.maximumSwap(2736));
+        Problem670 p = new Problem670();
+        System.out.println(p.maximumSwap(988357));
         stopWatch.stop();
         System.out.println(stopWatch.getTime() + "毫秒");
     }
-    
-    
-    
+
+    /**
+     * 单调栈求解
+     * @param num
+     * @return
+     */
     public int maximumSwap(int num) {
         char[] chars = String.valueOf(num).toCharArray();
         int[] res = new int[chars.length];
-        for (int i = 0; i < chars.length; i++) {
+        for (int i = 0; i < chars.length; ++i) {
             res[i] = Integer.parseInt(String.valueOf(chars[i]));
         }
-    
-        int[] ints = nextGreaterElement(res);
-        return 1;
-    }
-    
-    
-    public int[] nextGreaterElement(int[] nums) {
-        int n = nums.length;
-        int[] res = new int[n];
         Stack<Integer> stack = new Stack<>();
-        for (int i = n - 1; i >= 0; i--) {
-            while (!stack.isEmpty() && stack.peek() <= nums[i]) {
-                stack.pop();
+        int i = 0;
+        for (; i < res.length; i++) {
+            if (stack.isEmpty() || res[stack.peek()] >= res[i]) {
+                stack.push(i);
+            } else {
+                break;
             }
-            res[i] = stack.isEmpty() ? -1 : stack.peek();
-            stack.push(nums[i]);
         }
-        return res;
+        if (i == res.length) {
+            return num;
+        }
+
+        int left = 0;
+        int right = i;
+
+        for (; i < res.length; i++) {
+            if (res[i] >= res[right]) {
+                right = i;
+            }
+        }
+
+        while (!stack.isEmpty() && res[right] > res[stack.peek()]) {
+            left = stack.pop();
+        }
+
+
+        int temp = res[left];
+        res[left] = res[right];
+        res[right] = temp;
+
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int re : res) {
+            stringBuilder.append(re);
+        }
+
+
+        return Integer.parseInt(stringBuilder.toString());
     }
-    
-    
+
 
 }
--- src/main/java/leetcode/array/doing/Problem997.java
+++ src/main/java/leetcode/array/doing/Problem997.java
@@ -1,16 +1,25 @@
 package leetcode.array.doing;
 
-/**
+/**找到小镇的法官
  * @author : chezj
  * @date : 2022/9/13 10:02
  */
 public class Problem997 {
-    
-    
-    
-    
+
     public int findJudge(int n, int[][] trust) {
-        return 0;
+        int[] inDegrees = new int[n + 1];
+        int[] outDegrees = new int[n + 1];
+        for (int[] edge : trust) {
+            int x = edge[0], y = edge[1];
+            ++inDegrees[y];
+            ++outDegrees[x];
+        }
+        for (int i = 1; i <= n; ++i) {
+            if (inDegrees[i] == n - 1 && outDegrees[i] == 0) {
+                return i;
+            }
+        }
+        return -1;
     }
     
     
