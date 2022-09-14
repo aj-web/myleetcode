37828ac2a944fd5a55953332ffa17b866b3a2df1
chezhijun <1208370852@qq.com>
2022-09-15 00:05:32 +0800

    leetcode

--- src/main/java/leetcode/array/doing/Problem857.java
+++ src/main/java/leetcode/array/doing/Problem857.java
@@ -1,5 +1,7 @@
 package leetcode.array.doing;
 
+import java.util.Arrays;
+
 /**
  * @author : chezj
  * @date : 2022/9/11 14:31
@@ -7,13 +9,44 @@ package leetcode.array.doing;
 public class Problem857 {
 
 
-
+    /**
+     * 动态规划
+     * @param quality
+     * @param wage
+     * @param k
+     * @return
+     */
     public double mincostToHireWorkers(int[] quality, int[] wage, int k) {
+
+        int m= quality.length, n = quality.length;
+
+
+        int count = 0;
+        for (int i = 1; i < m; i++) {
+            int quai = quality[i];
+            for (int j = 0; j < i; j++) {
+                int quaj = quality[j];
+
+                int c = quai/quaj;
+
+                //以wagej为工资最小值
+                int wagi = wage[j]*c;
+                //以wagei为工资最小值
+                int wagj = quaj/c;
+                if (wagi>=wage[i]){
+
+                }
+
+
+
+
+            }
+        }
+
+
 
 
 
         return 0L;
-//        return null
-
     }
 }
