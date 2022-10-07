bb890cf254011bfb70d3acc194fadef9e8847a85
chezhijun <1208370852@qq.com>
2022-10-07 22:22:54 +0800

    leetcode

---
+++ src/main/java/leetcode/array/doing/Problem1784.java
@@ -0,0 +1,15 @@
+package leetcode.array.doing;
+
+/**
+ * @author : chezj
+ * @date : 2022/10/7 22:21
+ */
+public class Problem1784 {
+
+    public boolean checkOnesSegment(String s) {
+
+        String[] split = s.split("0");
+        return split.length == 1;
+
+    }
+}
