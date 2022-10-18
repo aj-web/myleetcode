c50b248a14b793334b7ce8f783c4fa569522676d
chezhijun <zhjche@rxhui.com>
2022-10-18 18:24:10 +0800

    leetcode  and Common.java

---
+++ src/main/java/DynamicProgramming/Problem902.java
@@ -0,0 +1,29 @@
+package DynamicProgramming;
+
+import org.apache.commons.lang3.time.StopWatch;
+
+/**
+ * @author : chezj
+ * @date : 2022/10/18 14:45
+ */
+public class Problem902 {
+    public static void main(String[] args) {
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+        Problem902  p = new Problem902();
+        
+        stopWatch.stop();
+        System.out.println(stopWatch.getTime() + "毫秒");
+    }
+    
+    public int atMostNGivenDigitSet(String[] digits, int n) {
+        
+    
+        
+        return 0;
+    }
+    
+    
+
+    
+}
