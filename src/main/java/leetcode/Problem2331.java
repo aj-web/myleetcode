b57e435d611bbac254d0e84a78bc5128ac170876
chezhijun <1208370852@qq.com>
2023-02-06 22:35:24 +0800

    leetcode

---
+++ src/main/java/leetcode/Problem2331.java
@@ -0,0 +1,53 @@
+package leetcode;
+
+import org.apache.commons.lang3.time.StopWatch;
+
+/**
+ * 计算布尔二叉树的值
+ *
+ * @author : chezj
+ * @date : 2023/2/6 22:13
+ * @TODO
+ */
+public class Problem2331 {
+
+    public static void main(String[] args) {
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+        Problem2331 p = new Problem2331();
+
+        stopWatch.stop();
+        System.out.println(stopWatch.getTime() + "毫秒");
+    }
+
+    public boolean evaluateTree(TreeNode root) {
+        if (root.left == null) {
+            return root.val == 1;
+        }
+        if (root.val == 2) {
+            return evaluateTree(root.left) || evaluateTree(root.right);
+        } else {
+            return evaluateTree(root.left) && evaluateTree(root.right);
+        }
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+}
