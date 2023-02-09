f3404bf9dee0df6f7c0cb6d3dc8e3dd992e3c8a5
chezhijun <zhjche@rxhui.com>
2023-02-09 18:15:02 +0800

    leetcode

---
+++ src/main/java/leetcode/array/Array1_500/Problem146.java
@@ -0,0 +1,69 @@
+package leetcode.array.Array1_500;
+
+import org.apache.commons.lang3.time.StopWatch;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * LRU缓存
+ *
+ * @author : chezj
+ * @date : 2023/2/9 16:46
+ */
+public class Problem146 {
+    
+    public static void main(String[] args) {
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+        Problem146 p = new Problem146();
+        
+        stopWatch.stop();
+        System.out.println(stopWatch.getTime() + "毫秒");
+    }
+    
+    
+}
+
+
+class LRUCache {
+    
+    int cap;
+    
+    Map<Integer, Integer> map;
+    
+    public LRUCache(int capacity) {
+        this.cap = capacity;
+        this.map = new LinkedHashMap<>(capacity);
+    }
+    
+    public int get(int key) {
+        if (!map.containsKey(key)) {
+            return -1;
+        }
+        makeRecently(key);
+        return map.get(key);
+    }
+    
+    public void put(int key, int value) {
+        if (map.containsKey(key)) {
+            map.put(key, value);
+            makeRecently(key);
+            return;
+        }
+        
+        if (map.size() >= cap) {
+            Integer head = map.keySet().iterator().next();
+            map.remove(head);
+        }
+        map.put(key, value);
+        
+    }
+    
+    //队尾
+    public void makeRecently(Integer key) {
+        Integer val = map.get(key);
+        map.remove(key);
+        map.put(key, val);
+    }
+}
