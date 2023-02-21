a2cbc41349247d6515d3c6d3f9bee9a2e3911bce
chezhijun <1208370852@qq.com>
2023-02-21 23:32:04 +0800

    leetcode

--- src/main/java/leetcode/LinkedList/LinkedList1_500/Problem92.java
+++ src/main/java/leetcode/LinkedList/LinkedList1_500/Problem92.java
@@ -3,6 +3,8 @@ package leetcode.LinkedList.LinkedList1_500;
 import org.apache.commons.lang3.time.StopWatch;
 
 /**
+ * 反转链表II
+ *
  * @author : chezj
  * @date : 2023/2/20 17:50
  */
@@ -18,9 +20,26 @@ public class Problem92 {
     }
 
     public ListNode reverseBetween(ListNode head, int left, int right) {
-        
+        if (left == 1) {
+            return reverseN(head, right);
+        }
+        head.next = reverseBetween(head.next, left - 1, right - 1);
+        return head;
+    }
 
-        return null;
+    ListNode successor = null; // 后驱节点
+
+    ListNode reverseN(ListNode head, int n) {
+        if (n == 1) {
+            successor = head.next;
+        }
+        // 以 head.next 为起点，需要反转前 n - 1 个节点
+        ListNode last = reverseN(head.next, n - 1);
+        head.next.next = head;
+        // 让反转之后的 head 节点和后面的节点连起来
+        head.next = successor;
+        return last;
+
     }
 
     private static class ListNode {
