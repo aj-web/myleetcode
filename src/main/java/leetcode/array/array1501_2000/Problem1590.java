3cc05a82dad8c72ef0aab996473d5654af0cf27c
chezhijun <1208370852@qq.com>
2023-03-13 23:45:47 +0800

    leetcode

--- src/main/java/leetcode/array/array1501_2000/Problem1590.java
+++ src/main/java/leetcode/array/array1501_2000/Problem1590.java
@@ -7,17 +7,17 @@ import org.apache.commons.lang3.time.StopWatch;
  * @date : 2023/3/9 16:06
  */
 public class Problem1590 {
-    
+
     public static void main(String[] args) {
         StopWatch stopWatch = new StopWatch();
         stopWatch.start();
         Problem1590 p = new Problem1590();
-        
+
         stopWatch.stop();
         System.out.println(stopWatch.getTime() + "毫秒");
     }
-    
+
     public int minSubarray(int[] nums, int p) {
-    
+        return 1;
     }
 }
