11b2d16541653d66e02b56f3e203553d24a6b9ca
chezhijun <zhjche@rxhui.com>
2023-05-17 17:32:08 +0800

    leetcode

--- src/main/java/leetcode/array/Problem704.java
+++ src/main/java/leetcode/array/Problem704.java
@@ -14,24 +14,53 @@ public class Problem704 {
     }
     
     
+    /**
+     * 左闭右闭
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
     public static int search(int[] nums, int target) {
         
-        if (nums.length == 0) {
+        if (target < nums[0] || target > nums[nums.length - 1]) {
             return -1;
         }
         
-        if (nums[0] == target) {
-            return 0;
-        }
+        int left = 0;
+        int right = nums.length - 1;
         
-        if (nums[nums.length - 1] == target) {
-            return nums.length - 1;
+        while (left <= right) {
+            int mid = left + ((right - left) >> 1);
+            if (nums[mid] < target) {
+                left = mid + 1;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+    
+    
+    /**
+     * 左闭右开：right在调整的时候不用取mid-1，因为取不到right，简单来说就是在[left,nums.length-2]闭区间做左闭右闭查询
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public static int searchfasa(int[] nums, int target) {
+        
+        if (target < nums[0] || target > nums[nums.length - 1]) {
+            return -1;
         }
         int left = 0;
         int right = nums.length - 1;
         
         while (left < right) {
-            int mid = left + (right - left) / 2;
+            int mid = left + ((right - left) >> 1);
             if (nums[mid] < target) {
                 left = mid + 1;
             } else if (nums[mid] > target) {
@@ -40,8 +69,34 @@ public class Problem704 {
                 return mid;
             }
         }
-        
         return -1;
+    }
+    
+    /**
+     * 二分查找查找最左边界 ：注意查找范围为[left，right]
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public static int binarySearchLeftUnite(int[] nums, int target) {
+        if (target < nums[0] || target > nums[nums.length - 1]) {
+            return -1;
+        }
+        int left = 0;
+        int right = nums.length - 1; // 注意
+        while (left <= right) {
+            int mid = left + ((right - left) >> 1);
+            if (nums[mid] == target) {
+                right = mid - 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            }
+        }
+        
+        return left;
     }
     
     
@@ -59,7 +114,7 @@ public class Problem704 {
         int left = 0;
         int right = nums.length; // 注意
         while (left < right) {
-            int mid = left + (right - left) / 2;
+            int mid = left + ((right - left) >> 1);
             if (nums[mid] == target) {
                 right = mid;
             } else if (nums[mid] < target) {
@@ -69,62 +124,6 @@ public class Problem704 {
             }
         }
         
-        return left;
-    }
-    
-    
-    /**
-     * 二分查找查找最左边界 ：注意查找范围为[left，right]
-     *
-     * @param nums
-     * @param target
-     * @return
-     */
-    public static int binarySearchLeftUnite(int[] nums, int target) {
-        if (nums.length == 0) {
-            return -1;
-        }
-        int left = 0;
-        int right = nums.length - 1; // 注意
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (nums[mid] == target) {
-                right = mid - 1;
-            } else if (nums[mid] < target) {
-                left = mid + 1;
-            } else if (nums[mid] > target) {
-                right = mid - 1;
-            }
-        }
-        
-        return left;
-    }
-    
-    
-    /**
-     * 二分查找查找最右边界 ：注意查找范围为[left，right)
-     *
-     * @param nums
-     * @param target
-     * @return
-     */
-    public static int binarySearchRight(int[] nums, int target) {
-        if (nums.length == 0) {
-            return -1;
-        }
-        int left = 0;
-        int right = nums.length; // 注意
-        while (left < right) {
-            int mid = left + (right - left) / 2;
-            if (nums[mid] == target) {
-                left = mid + 1;
-            } else if (nums[mid] < target) {
-                left = mid + 1;
-            } else if (nums[mid] > target) {
-                //这里不能收缩，因为右侧为开区间，如果，mid-1的话，那么指针就直接指向3，漏了一个数6    {1,.....,3,6,8}
-                right = mid;
-            }
-        }
         return left;
     }
     
@@ -143,7 +142,7 @@ public class Problem704 {
         int left = 0;
         int right = nums.length - 1; // 注意
         while (left < right) {
-            int mid = left + (right - left) / 2;
+            int mid = left + ((right - left) >> 1);
             if (nums[mid] == target) {
                 left = mid + 1;
             } else if (nums[mid] < target) {
@@ -151,6 +150,33 @@ public class Problem704 {
             } else if (nums[mid] > target) {
                 //这里可以收缩边界
                 right = mid - 1;
+            }
+        }
+        return left;
+    }
+    
+    /**
+     * 二分查找查找最右边界 ：注意查找范围为[left，right)
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public static int binarySearchRight(int[] nums, int target) {
+        if (nums.length == 0) {
+            return -1;
+        }
+        int left = 0;
+        int right = nums.length; // 注意
+        while (left < right) {
+            int mid = left + ((right - left) >> 1);
+            if (nums[mid] == target) {
+                left = mid + 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else if (nums[mid] > target) {
+                //这里不能收缩，因为右侧为开区间，如果，mid-1的话，那么指针就直接指向3，漏了一个数6    {1,.....,3,6,8}
+                right = mid;
             }
         }
         return left;
